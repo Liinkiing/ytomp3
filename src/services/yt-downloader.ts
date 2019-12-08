b53d97aa0e5fc55cfc1600a4dd3dc584d3a50313
Omar Jbara <omar.jbara2@gmail.com>
2019-12-08 19:47:22 +0100

    - Added YTDownloader service to handle logic

---
+++ src/services/yt-downloader.ts
@@ -0,0 +1,41 @@
+import ffpmegInstaller from '@ffmpeg-installer/ffmpeg'
+import ffmpeg from 'fluent-ffmpeg'
+import ytdl from 'ytdl-core'
+
+interface DownloadParams {
+  uri: string;
+  output: string;
+  bitrate?: number;
+}
+
+class YTDownloader {
+  constructor(options: { ffmpeg: { path: string; version: string } }) {
+    ffmpeg
+    .setFfmpegPath(options.ffmpeg.path)
+  }
+
+  public download = async ({uri, output, bitrate = 128}: DownloadParams) => {
+    return new Promise((resolve, reject) => {
+      const video = ytdl(uri)
+      ffmpeg(video)
+      .on('progress', progress => {
+        console.log(progress.timemark)
+      })
+      .on('end', () => {
+        resolve()
+      })
+      .on('error', error => {
+        reject(error.message)
+      })
+      .audioBitrate(bitrate)
+      .save(output)
+    })
+  }
+}
+
+export default new YTDownloader({
+  ffmpeg: {
+    path: ffpmegInstaller.path,
+    version: ffpmegInstaller.version,
+  },
+})
